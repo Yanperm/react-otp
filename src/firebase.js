ab666b64d3548a48d581b42b31c5241ad9572e8d
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2021-12-26 22:14:25 +0700

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-<<<<<<< HEAD
-  apiKey: "AIzaSyDYwelHcOWIP6lD7A-SHQwZtddCC1_G_Yo",
-  authDomain: "softubonlimitedpartnership.firebaseapp.com",
-  projectId: "softubonlimitedpartnership",
-  storageBucket: "softubonlimitedpartnership.appspot.com",
-  messagingSenderId: "805342429419",
-  appId: "1:805342429419:web:b151f6af8b5f0319797eae"
-=======
-    apiKey: "AIzaSyBmk0D4gRRX_2pW_GtQQsw3jekROlT5DVg",
-    authDomain: "otp-app-demo-6770f.firebaseapp.com",
-    projectId: "otp-app-demo-6770f",
-    storageBucket: "otp-app-demo-6770f.appspot.com",
-    messagingSenderId: "32572480615",
-    appId: "1:32572480615:web:41f9d17ee13b08036708c2"
->>>>>>> parent of 2001b0b5 (Removed firebase config)
+ 
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
